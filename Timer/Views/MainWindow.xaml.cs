8fe9cca1994778363dca96bcc0af5dbcac7d47ea
try.again.31.06@gmail.com <try.again.31.06@gmail.com>
2022-03-27 00:34:01 +0900

    スタート、ストップ、リセット機能追加

--- Timer/Views/MainWindow.xaml.cs
+++ Timer/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
-//uehara
-
 namespace Timer.Views
 {
     /// <summary>
@@ -23,24 +22,57 @@ namespace Timer.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Stopwatch stopwatch = new();
+        //private readonly EventHandler Timer_Tick;
+
         public MainWindow()
         {
 
             InitializeComponent();
 
+            //定期的に実行
+            DispatcherTimer timer = new();
+            //0時間、0分、1秒ごとに動作
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            this.stopwatch.Stop();
         }
 
-        
-
-        private void Button_Start(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 時間が表示されるところの処理を記載した関数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
         {
-
             
+            int totalSecounds = ((int)this.stopwatch.Elapsed.TotalSeconds);
+
+            TimeSpan countTime = new(0, 0,totalSecounds);
+            timerTextBlock.Text = countTime.ToString(@"mm\:ss");
         }
 
-        private void Button_StopAndReset(object sender, RoutedEventArgs e)
+        private void Button_StartAndStop(object sender, RoutedEventArgs e)
         {
+            if (stopwatch.IsRunning)
+            {
+                stopwatch.Stop();
+                btnStartAndStop.Content = "Start";
+            }
+            else
+            {
+                stopwatch.Start();
+                btnStartAndStop.Content = "Stop";
+            }
 
+        }
+
+        private void Button_Reset(object sender, RoutedEventArgs e)
+        {
+            stopwatch.Restart();
+            this.stopwatch.Stop();
         }
 
         private void Button_Save(object sender, RoutedEventArgs e)
